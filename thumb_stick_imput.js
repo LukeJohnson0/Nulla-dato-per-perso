3aa71ded6f694752e4c4f36ab53613b00b03f8f3
photonB3am <106384554+LukeJohnson0@users.noreply.github.com>
2026-02-27 00:26:34 +0100

    Update and rename char_movement.js to thumb_stick_imput.js

---
+++ thumb_stick_imput.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', loaded => {
+	var shape;
+	function drawUI() {
+		if(loaded) {
+			shape = document.getElementById("inputUI");
+			const ctx = shape.getContext("2d");
+			ctx.beginPath();
+			ctx.arc(75, 100, 40, 0, 2 * Math.PI);
+			ctx.fillStyle = 'white';
+			ctx.fill()
+		}
+	}
+	drawUI();
+	class updateInput {
+		constructor(touchIntX, touchIntY, onClicked) { // constructor that contains variables that can
+			this.touchIntX = touchIntX; // accessed through another file.
+			this.touchIntY = touchIntY;
+			this.onClicked = onClicked = false;
+			shape.addEventListener( /*'ontouchstart',*/ "pointerdown", onClick => {});
+			setTimeout(() => {
+				const updateLeaveTouch = (onClick) => {
+					switch(onClick.type) {
+						case "pointerdown":
+							this.onClicked = true;
+							break;
+						default:
+							this.onClicked = false;
+							break;
+					}
+				}
+			}, 3.2);
+			window.addEventListener(( /*"touchmove", */ "mousemove"), clientInput => {
+				this.touchIntX = clientInput.clientX;
+				this.touchIntY = clientInput.clientY;
+			})
+		}
+		updateFunc() {
+			console.log(`${this.onClicked}`);
+			if(this.onClicked) {
+				console.log(`Position: ${this.touchIntX}, ${this.touchIntY}`);
+				shape.style.top = this.touchIntY + 'px';
+				shape.style.right = this.touchIntX + 'px';
+			}
+			/*console.log("Update working!"); debug passed: it works*/
+		}
+	}
+	if(shape) {
+		var input = new updateInput(0, 0);
+	}
+
+	function updateFunc() {
+		requestAnimationFrame(updateFunc);
+		input.updateFunc(); //:void
+	}
+	updateFunc();
+});
