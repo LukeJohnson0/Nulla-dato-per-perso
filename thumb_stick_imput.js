2eaa2e74b004b50d8d886461308e7c44d330cdbf
photonB3am <106384554+LukeJohnson0@users.noreply.github.com>
2026-03-13 22:57:49 +0100

    Refactor thumb stick input handling and UI drawing

--- thumb_stick_imput.js
+++ thumb_stick_imput.js
@@ -5,112 +5,113 @@
 document.addEventListener('DOMContentLoaded', loaded => {
     var shape;
     var input;
-    const init={ 
-		x:300 + "px",
-		y:300 + "px",
-	}
-	init.boundYPos = Number((init.y.replace("px", ""))) + 70 + "px";
-	init.boundYMin = Number((init.y.replace("px", ""))) - 70 + "px";
-	init.boundXPos = Number((init.x.replace("px", ""))) + 70 + "px";
-	init.boundXMin = Number((init.x.replace("px", ""))) - 70 + "px";
+    const init = {
+        x: 300,
+        y: 300,
+        radius: 70
+    }
+    const usrAgent = /iPhone|iPad/.test(navigator.userAgent);
     function drawUI() {
-        if(loaded) {
+        if (loaded) {
             shape = document.getElementById("inputUI");
             const ctx = shape.getContext("2d");
-			bound = document.getElementById("boundRec");
-			const ctx2 = bound.getContext("2d");
-			ctx2.transform(1,0,0,1,-9,-9);
-			ctx2.fillStyle = "white";
-			ctx2.fillRect(10,10, 200,200);
-			/* transform is represented in this matrix:
-			┌ 		  ┐		┌ 		   ┐
-			│ a  c  e │		│ 1  0  25 │
-			│ b  d  f │ =>  │ 0  1  0  │
-			│ 0  0  1 │		│ 0  0  1  │
-			└ 		  ┘		└ 		   ┘
-			*/
-			ctx.transform(1,0,0,1,25,0);
+            bound = document.getElementById("boundRec");
+            const ctx2 = bound.getContext("2d");
+            /* transform is represented in this matrix:
+            ┌ 		  ┐		┌ 		   ┐
+            │ a  c  e │		│ 1  0  25 │
+            │ b  d  f │ =>  │ 0  1  0  │
+            │ 0  0  1 │		│ 0  0  1  │
+            └ 		  ┘		└ 		   ┘ */
+            ctx.transform(1, 0, 0, 1, 25, 0);
             ctx.beginPath();
             ctx.arc(75, 100, 40, 0, 2 * Math.PI);
-			shape.style.x = bound.style.x = init.x;
-			shape.style.y = bound.style.y = init.y;
             ctx.fillStyle = "white";
             ctx.fill();
+            ctx2.strokeStyle = "rgba(255, 255, 255, 0.3)";
+            ctx2.lineWidth = 2;
+            ctx2.beginPath();
+            ctx2.arc(init.x, init.y, init.radius, 0, 2 * Math.PI);
+            ctx2.stroke();
+            shape.style.left = bound.style.left = init.x + "px";
+            shape.style.top = bound.style.top = init.y + "px";
         }
     }
-    
+
     drawUI();
     class updateInput {
-        constructor(touchIntX, touchIntY, onClicked) {
+        constructor (touchIntX, touchIntY, onClicked) {
             this.touchIntX = touchIntX;
             this.touchIntY = touchIntY;
-            this.onClicked = false;
+            this.onClicked = onClicked = false;
             this.boundRectX = 0;
-			this.boundRectY = 0;	
-			this.boundLimit = bound.getBoundingClientRect();	
-			this.limit = false;
+            this.boundRectY = 0;
             const inputMap = {
                 'touchstart': true,
-                'touchend' : false,
-                'mousedown' : true,
-                'mouseup' : false,
-                'pointerleave' : false
+                'touchend': false,
+                'mousedown': true,
+                'mouseup': false,
+                'pointerleave' : false,
+                "touchcancel" : false
             };
-            
+
             Object.entries(inputMap).forEach(([eventType, state]) => {
                 shape.addEventListener(eventType, (e) => {
-					var boundRect = shape.getBoundingClientRect();
-					this.boundRectX = e.clientX - boundRect.left;
-					this.boundRectY =  e.clientY - boundRect.top;
+                    e.preventDefault();
+                    var boundRect = shape.getBoundingClientRect();
+                    this.boundRectX = (e.touches ? e.touches[0].clientX : e.clientX ) - boundRect.left;
+                    this.boundRectY = (e.touches ? e.touches[0].clientY : e.clientY) - boundRect.top;
                     this.onClicked = state;
-                });
+                }, {passive:false});
             });
-            
-            window.addEventListener("mousemove", clientInput => {
-                this.touchIntX = clientInput.clientX;
-                this.touchIntY = clientInput.clientY;
-            });
+            if(usrAgent){
+                    console.log("phgone");
+                    shape.addEventListener("touchmove", clientInput => {
+                        clientInput.preventDefault();
+                    this.touchIntX = clientInput.touches.item(0).clientX;
+                    this.touchIntY = clientInput.touches.item(0).clientY;},{passive:false});
+                    // console.log(this.touchIntY); console.log(this.touchIntX);
+            }
+            else{
+                    window.addEventListener("mousemove", clientInput => {
+                    this.touchIntX = clientInput.clientX;
+                    this.touchIntY = clientInput.clientY;});
+            }
         }
-        limitBound(){
-			var lastPos = {
-				x: 200 + "px",// shape.style.x,
-				y:shape.style.y
-			}
-			if(shape.style.left >= init.boundXPos | shape.style.left <= init.boundXMin |
-				shape.style.top <= init.boundYMin| shape.style.top >= init.boundYPos){
-					this.limit = true;
-					console.log("out of bounds!");
-					shape.style.x = lastPos.x;
-					shape.style.y = lastPos.y;
-			}
-			else{
-                setTimeout(()=>{this.limit = false;}, .6);
-			}
-		}
+        outOfBound(x, y, radius) {
+            let distance = Math.sqrt(((x - init.x) ** 2) + ((y - init.y) ** 2));
+            if (distance > radius) {
+                let teta = Math.atan2(y - init.y, x - init.x);
+                return {
+                    x: init.x + radius * Math.cos(teta),
+                    y: init.y + radius * Math.sin(teta)
+                }
+            }
+            return {x, y};
+        }
         callUPD() {
-            //console.log(`left:${shape.style.left}, top:${shape.style.top}`);
-            //console.log(this.limit);
-            this.limitBound();
-            if(this.onClicked && !this.limit) {
-                //console.log(`Position: ${this.touchIntX}, ${this.touchIntY}`);
-                shape.style.left = (this.touchIntX - this.boundRectX)  + "px";
-                shape.style.top = (this.touchIntY - this.boundRectY) + "px";
+            if (this.onClicked) {
+                let xGoal = this.touchIntX - this.boundRectX;
+                let yGoal = this.touchIntY - this.boundRectY;
+                const final = this.outOfBound(xGoal, yGoal, init.radius);
+                shape.style.left = final.x + "px";
+                shape.style.top = final.y + "px";
                 //console.log(`UI: ${shape.style.top}, ${shape.style.left}`);
             }
-			else if(!this.onClicked){
-				shape.style.left = bound.style.left = init.x;
-				shape.style.top =bound.style.top  =init.y;
-			}
+            else {
+                shape.style.left = bound.style.left = init.x + "px";
+                shape.style.top = bound.style.top = init.y + "px";
+            }
         }
     }
-    
-    if(shape) {
+
+    if (shape) {
         input = new updateInput(0, 0, false);
     }
 
     function updateFunc() {
         requestAnimationFrame(updateFunc);
-        if(input) input.callUPD();
+        if (input) input.callUPD();
     }
     updateFunc();
 });
